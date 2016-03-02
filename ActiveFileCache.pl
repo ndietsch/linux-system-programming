220ff2912eb8126b3d4cf19e9935ac00d82991d4
EC2 Default User <ec2-user@ip-172-31-37-189.us-west-2.compute.internal>
2016-03-02 05:26:45 +0000

    Added extra prinouts

--- ActiveFileCache.pl
+++ ActiveFileCache.pl
@@ -10,14 +10,16 @@ printf("%-20s\t%s\n","Active(anon)",$mem{"Active(anon)"});
 while() {
 	sleep 1;
 	$count++;
-	# Create a 5MB file full of zeros and then read it to fill the Active(file) cache from /proc/meminfo
-	my $retval=system("/bin/dd if=/dev/zero of=/tmp/file.${count} count=10240 > /dev/null 2>&1");
+	# Create a 10MB file full of zeros and then read it to fill the Active(file) cache from /proc/meminfo
+	my $retval=system("/bin/dd if=/dev/zero of=/tmp/file.${count} count=20480 > /dev/null 2>&1");
 	open(my $fh, "</tmp/${count}");
 	while ( <$fh>){}
 
 	# Get memory statistics after creating and reading a 5MB file, watch Active(file) grow until it is 5MB
 	%mem = get_mem_info;
 	printf("%-20s\t%s\n","Active(file)",$mem{"Active(file)"});
+	printf("%-20s\t%s\n","Inactive(file)",$mem{"Inactive(file)"});
+	printf("%-20s\t%s\n","MemFree",$mem{"MemFree"});
 }
 	
 
